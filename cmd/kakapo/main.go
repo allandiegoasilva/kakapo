98b45d059dfb788ac700b75141dbba3ec7f5c6e4
Allan Diego <34951453+allandiegoasilva@users.noreply.github.com>
2026-07-21 11:58:43 -0300

    adjust commitlintrc

--- cmd/kakapo/main.go
+++ cmd/kakapo/main.go
@@ -11,6 +11,7 @@ func main() {
 
 	router.GET("/", func(c *gin.Context) {
 		fmt.Println(" entrou aq")
+		fmt.Printf("O vet vai pegar isso: %d", "uma string")
 
 		c.JSON(200, gin.H{
 			"message": "Hello, World!",
