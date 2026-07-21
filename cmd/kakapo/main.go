63e4d785d6758a09f093bc87b1feff3e37f74083
Allan Diego <34951453+allandiegoasilva@users.noreply.github.com>
2026-07-21 11:53:51 -0300

    test

--- cmd/kakapo/main.go
+++ cmd/kakapo/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
+		fmt.Println(" entrou aq")
+
 		c.JSON(200, gin.H{
 			"message": "Hello, World!",
 		})
