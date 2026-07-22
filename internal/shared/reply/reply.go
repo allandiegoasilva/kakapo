962b6c0c84e0550ba943bb4e1a007046678cf339
Allan Diego <34951453+allandiegoasilva@users.noreply.github.com>
2026-07-22 18:01:19 -0300

    refactor: standardize formatting of HTTP status constants in reply.go

--- internal/shared/reply/reply.go
+++ internal/shared/reply/reply.go
@@ -3,12 +3,12 @@ package reply
 type Empty struct{}
 
 const (
-	OK                   = 200
-	ValidationError      = 400
-	Unauthorized         = 401
-	Forbidden            = 403
-	NotFound             = 404
-	InternalServerError  = 500
+	OK                  = 200
+	ValidationError     = 400
+	Unauthorized        = 401
+	Forbidden           = 403
+	NotFound            = 404
+	InternalServerError = 500
 )
 
 type Reply[T Empty] struct {
